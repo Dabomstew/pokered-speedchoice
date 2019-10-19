2b6d80dcc989a0b56000b960afceb9069e20c08b
Dabomstew <4035468+Dabomstew@users.noreply.github.com>
2019-10-19 14:43:43 +1300

    Remove route 2 cut bush if key item rando

--- scripts/Route2.asm
+++ scripts/Route2.asm
@@ -1,5 +1,19 @@
 Route2_Script:
+	call Route2_RemoveCutBush
 	jp EnableAutoTextBoxDrawing
+	
+Route2_RemoveCutBush:
+	ld hl, wCurrentMapScriptFlags
+	bit 5, [hl]
+	res 5, [hl]
+	ret z
+	ldafarbyte KeyItemRandoActive
+	and a
+	ret z
+	ld a, $6D ; no cut bush
+	ld [wNewTileBlockID], a
+	lb bc, $0B, $07 ; y, x
+	predef_jump ReplaceTileBlock
 
 Route2_TextPointers:
 	dw PickUpItemText
